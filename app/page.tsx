ecc9a1f1cbf2e672afffa25801eef8055818b9d1
fassiliss <fassiliss@gmail.com>
2025-09-22 19:33:47 -0500

    implement ESLint & Prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
     <div>
-      <h1 className="text-3xl text-violet-700 font-black">Welcome to Next.js</h1>
+      <h1 className="text-3xl font-black text-violet-700">
+        Welcome to Next.js
+      </h1>
     </div>
-  )
-}
+  );
+};
 
 export default Home;
