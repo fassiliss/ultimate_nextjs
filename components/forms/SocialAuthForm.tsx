9479379380639d40e9084c00fdd9b46f479c1c81
fassiliss <fassiliss@gmail.com>
2025-10-22 22:37:41 -0500

    Socialauth

--- components/forms/SocialAuthForm.tsx
+++ components/forms/SocialAuthForm.tsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
+import { Button } from "../ui/button";
 
 const SocialAuthForm = () => {
   return (
-    <div>
-      SocialAuthForm
+    <div className="mt-10 flex flex-wrap gap-2.5">
+      <Button className="background-dark400_light900">
+        <Image
+          src="/icons/github.svg"
+          alt="GitHub Logo"
+          width={20}
+          height={20}
+          className="invert-colors mr-2.5 object-contain"
+        />
+      </Button>
     </div>
-  )
-}
+  );
+};
 
-export default SocialAuthForm
+export default SocialAuthForm;
